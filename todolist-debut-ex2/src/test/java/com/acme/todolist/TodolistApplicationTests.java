c87c8fbbcc9822527bb3d397549fd79b17e7fa5d
Aboubacar23 <aboubacarsidikiconde23@gmail.com>
2023-04-07 23:34:34 +0200

    ajout du test unit

--- todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
@@ -1,13 +1,27 @@
 package com.acme.todolist;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.acme.todolist.configuration.TodolistApplication;
+import com.acme.todolist.domain.TodoItem;
+
+@SpringBootTest(classes=TodolistApplication.class)
+@RunWith(SpringRunner.class)
 class TodolistApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
+			// Créer un objet TodoItem avec une date
+			TodoItem item = new TodoItem("1", Instant.now().minus(2, ChronoUnit.DAYS), "Todo");
+			// l'élément est en retard
+			assertEquals("[LATE!]", item.finalContent());
+		}
 }
